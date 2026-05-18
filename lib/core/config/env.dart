53a23b358e85c901ea65371467bd9ec9664c0923
Rohith S <rohithsivakumar1205@gmail.com>
2026-05-18 18:56:54 +0530

    securing api keys

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,17 +1,28 @@
-/// Envied configuration simulation/wrapper for environment variables.
-/// In a standard workflow with build_runner, this would be generated via @Envied().
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+/// Environment configuration wrapper loading securely from dotenv.
 class Env {
-  static const String revenueCatApiKeyAndroidDev = 'goog_dev_mock_revenuecat_api_key';
-  static const String revenueCatApiKeyAndroidStaging = 'goog_staging_mock_revenuecat_api_key';
-  static const String revenueCatApiKeyAndroidProd = 'goog_prod_mock_revenuecat_api_key';
+  static String get revenueCatApiKeyAndroidDev => 
+      dotenv.env['REVENUECAT_API_KEY_ANDROID_DEV'] ?? 'goog_dev_mock_revenuecat_api_key';
+  static String get revenueCatApiKeyAndroidStaging => 
+      dotenv.env['REVENUECAT_API_KEY_ANDROID_STAGING'] ?? 'goog_staging_mock_revenuecat_api_key';
+  static String get revenueCatApiKeyAndroidProd => 
+      dotenv.env['REVENUECAT_API_KEY_ANDROID_PROD'] ?? 'goog_prod_mock_revenuecat_api_key';
 
-  static const String revenueCatApiKeyIosDev = 'appl_dev_mock_revenuecat_api_key';
-  static const String revenueCatApiKeyIosStaging = 'appl_staging_mock_revenuecat_api_key';
-  static const String revenueCatApiKeyIosProd = 'appl_prod_mock_revenuecat_api_key';
+  static String get revenueCatApiKeyIosDev => 
+      dotenv.env['REVENUECAT_API_KEY_IOS_DEV'] ?? 'appl_dev_mock_revenuecat_api_key';
+  static String get revenueCatApiKeyIosStaging => 
+      dotenv.env['REVENUECAT_API_KEY_IOS_STAGING'] ?? 'appl_staging_mock_revenuecat_api_key';
+  static String get revenueCatApiKeyIosProd => 
+      dotenv.env['REVENUECAT_API_KEY_IOS_PROD'] ?? 'appl_prod_mock_revenuecat_api_key';
 
-  static const String supabaseAnonKeyDev = 'dev_anon_key_mock_2026';
-  static const String supabaseAnonKeyStaging = 'staging_anon_key_mock_2026';
-  static const String supabaseAnonKeyProd = 'prod_anon_key_mock_2026';
+  static String get supabaseAnonKeyDev => 
+      dotenv.env['SUPABASE_ANON_KEY_DEV'] ?? 'dev_anon_key_mock_2026';
+  static String get supabaseAnonKeyStaging => 
+      dotenv.env['SUPABASE_ANON_KEY_STAGING'] ?? 'staging_anon_key_mock_2026';
+  static String get supabaseAnonKeyProd => 
+      dotenv.env['SUPABASE_ANON_KEY_PROD'] ?? 'prod_anon_key_mock_2026';
 
-  static const String aiEngineApiKey = 'ai_engine_mock_secret_key_2026';
+  static String get aiEngineApiKey => 
+      dotenv.env['AI_ENGINE_API_KEY'] ?? 'ai_engine_mock_secret_key_2026';
 }
--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
@@ -22,36 +23,36 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyMockKeyForWebSyncHabit12345',
-    appId: '1:1234567890:web:abcdef123456',
-    messagingSenderId: '1234567890',
-    projectId: 'synchabit-cyber-os',
+  static FirebaseOptions get web => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_WEB'] ?? 'your_firebase_web_api_key',
+    appId: dotenv.env['FIREBASE_APP_ID_WEB'] ?? '1:1234567890:web:abcdef123456',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_WEB'] ?? '1234567890',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID_WEB'] ?? 'synchabit-cyber-os',
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCLd-Ete1-E7tZJY-8eRLX7onWABRn_xuA',
-    appId: '1:182981373013:android:5156fabbe2daa52f2d78dc',
-    messagingSenderId: '182981373013',
-    projectId: 'synchabit-4cfa4',
-    storageBucket: 'synchabit-4cfa4.firebasestorage.app',
+  static FirebaseOptions get android => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_ANDROID'] ?? 'your_firebase_android_api_key',
+    appId: dotenv.env['FIREBASE_APP_ID_ANDROID'] ?? '1:182981373013:android:5156fabbe2daa52f2d78dc',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_ANDROID'] ?? '182981373013',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID_ANDROID'] ?? 'synchabit-4cfa4',
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_ANDROID'] ?? 'synchabit-4cfa4.firebasestorage.app',
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBv0iG-8JdV8EwxzrXY2oVkhVV86U7jcqs',
-    appId: '1:182981373013:ios:658e7a191eb0f6d52d78dc',
-    messagingSenderId: '182981373013',
-    projectId: 'synchabit-4cfa4',
-    storageBucket: 'synchabit-4cfa4.firebasestorage.app',
-    iosBundleId: 'com.synchabit.synchabit',
+  static FirebaseOptions get ios => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_IOS'] ?? 'your_firebase_ios_api_key',
+    appId: dotenv.env['FIREBASE_APP_ID_IOS'] ?? '1:182981373013:ios:658e7a191eb0f6d52d78dc',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_IOS'] ?? '182981373013',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID_IOS'] ?? 'synchabit-4cfa4',
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_IOS'] ?? 'synchabit-4cfa4.firebasestorage.app',
+    iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID'] ?? 'com.synchabit.synchabit',
   );
 
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyMockKeyForMacosSyncHabit12345',
-    appId: '1:1234567890:ios:abcdef123456',
-    messagingSenderId: '1234567890',
-    projectId: 'synchabit-cyber-os',
-    storageBucket: 'synchabit-cyber-os.appspot.com',
-    iosBundleId: 'com.synchabit.app',
+  static FirebaseOptions get macos => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_MACOS'] ?? 'your_firebase_macos_api_key',
+    appId: dotenv.env['FIREBASE_APP_ID_MACOS'] ?? '1:1234567890:ios:abcdef123456',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_MACOS'] ?? '1234567890',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID_MACOS'] ?? 'synchabit-cyber-os',
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_MACOS'] ?? 'synchabit-cyber-os.appspot.com',
+    iosBundleId: dotenv.env['FIREBASE_MACOS_BUNDLE_ID'] ?? 'com.synchabit.app',
   );
 }
